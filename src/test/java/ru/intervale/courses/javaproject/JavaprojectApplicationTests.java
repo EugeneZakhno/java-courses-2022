4857bf5babb96c6233980a32317e2a60b812749c
Eugene Zakhno <zohan1234567@gmail.com>
2021-12-27 23:08:29 +0300

    init

---
+++ src/test/java/ru/intervale/courses/javaproject/JavaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.intervale.courses.javaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
